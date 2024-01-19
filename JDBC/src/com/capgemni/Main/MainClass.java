106f6eb5a096b82ad17ea39bd1c5b1762b23217d
Anjali <Anjali@172.25.4.45>
2024-01-19 10:47:52 +0530

    Commit

---
+++ JDBC/src/com/capgemni/Main/MainClass.java
@@ -0,0 +1,11 @@
+package com.capgemni.Main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
